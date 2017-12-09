c20955798c8502c93fabe64b8ed1ad6528b45ce2
sunshinelsonZenBook <sunshinelson@gmail.com>
2017-12-09 05:43:40 +0100

    Uebung 2 bereinigt

--- Uebung3/Expression/Vector.h
+++ Uebung3/Expression/Vector.h
@@ -3,14 +3,30 @@
 #include <array>
 using namespace std;
 
-template< class T, size_t S> class Vector : public array<T, S>{
+template <class T, size_t S> class Vector : public array<T, S>{
 private:
 
 public:
-	
+	//Standardconstruktor
+	Vector(
+		size_t s = 0;
+	);
+	//Konstruktor mit Initialisierungslisten
 	Vector(const std::initializer_list<T>& data) {
 		size_t s = __min(data.size(), S);
 		auto it = data.begin();
 		for (size_t i = 0; i < s; i++) this->at(i) = *it++;
-	}
+	};
+
+	// Ausgabe-Operator für Output-Streams
+	friend ostream& operator<<(ostream& os, const Vector& v) {
+		auto vptr = v.begin();
+		os << "[";
+		if (!v.isEmpty()) os << vptr[0];
+		for (size_t i = 1; i < S; ++i) { os << ", " << vptr[i]; }
+		os << "]";
+		return os;
+	};
+
+	~Vector();
 };
