21e101eef2ff7d0302ba22fbcba13512349bc6a1
sunshinelsonZenBook <sunshinelson@gmail.com>
2018-01-07 20:49:11 +0100

    Uebung 1

--- Uebung3/Expression/Expression.h
+++ Uebung3/Expression/Expression.h
@@ -34,35 +34,114 @@ class Expression {
 public:
 	typedef typename Left::value_type value_type;
 
-	Expression(const Left& l, const Right& r) : m_left{ l }, m_right{ r } {};
+	Expression(const Left& l, const Right& r) : m_left{ l }, m_right{ r } {}
 
-	size_t size() const { return m_left.size(); };
+	size_t size() const { return m_left.size(); }
 
 	// Ausgabe-Operator für Output-Streams	
 	friend ostream& operator<<(ostream& os, const Expression& e) {
 		size_t s = e.m_left.size();
-		os << "[";
-		for (size_t i = 0; i < s; ++i) { os << ", " << e[i]; }
-		os << "]";
+		os << "e[";
+		for (size_t i = 0; i < s; ++i) { os << "  " << e[i]; }
+		os << "  ]";
 		return os;
-	};
+	}
 
+	// Indexoperator
 	value_type operator[](int i) const {
 		return Op::apply<value_type>(m_left[i], m_right[i]);
-	};
+	}
 
 	//Gleichheitsoperator
-	bool operator==(const Expression& e) const {
+	template<typename T>
+	bool operator==(const T& t) const {
+		for (size_t i = 0; i < m_left.size(); i++){
+				if (this->operator[](i) != t[i]) {
+					return false;
+				}
+		}
+		return true;
+	}
+
+};
+
+template <typename Left, typename Op, typename Right = double>
+class Expression {
+	const Left& m_left;
+	const Right& m_right;
+
+public:
+	typedef typename Left::value_type value_type;
+
+	Expression(const Left& l, const Right& r) : m_left{ l }, m_right{ r } {}
+
+	size_t size() const { return m_left.size(); }
+
+	// Ausgabe-Operator für Output-Streams	
+	friend ostream& operator<<(ostream& os, const Expression& e) {
+		size_t s = e.m_left.size();
+		os << "e[";
+		for (size_t i = 0; i < s; ++i) { os << "  " << e[i]; }
+		os << "  ]";
+		return os;
+	}
+
+	// Indexoperator Spezialisierung 1
+	value_type operator[](int i) const {
+		return Op::apply<value_type>(m_left[i], m_right);
+	}
+
+	//Gleichheitsoperator
+	template<typename T>
+	bool operator==(const T& t) const {
 		for (size_t i = 0; i < m_left.size(); i++) {
-			if (this[i] != e[i]) {
+			if (this->operator[](i) != t[i]) {
 				return false;
 			}
 		}
 		return true;
-	};
+	}
 
 };
 
+template <typename Left=double, typename Op, typename Right>
+class Expression {
+	const Left& m_left;
+	const Right& m_right;
+
+public:
+	typedef typename Right::value_type value_type;
+
+	Expression(const Left& l, const Right& r) : m_left{ l }, m_right{ r } {}
+
+	size_t size() const { return m_right.size(); }
+
+	// Ausgabe-Operator für Output-Streams	
+	friend ostream& operator<<(ostream& os, const Expression& e) {
+		size_t s = e.m_left.size();
+		os << "e[";
+		for (size_t i = 0; i < s; ++i) { os << "  " << e[i]; }
+		os << "  ]";
+		return os;
+	}
+
+	// Indexoperator Spezialisierung 2
+	value_type operator[](int i) const {
+		return Op::apply<value_type>(m_left, m_right[i]);
+	}
+
+	//Gleichheitsoperator
+	template<typename T>
+	bool operator==(const T& t) const {
+		for (size_t i = 0; i < m_left.size(); i++) {
+			if (this->operator[](i) != t[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+};
 
 template<typename Left, typename Right>
 Expression<Left, Add, Right> operator+(const Left& l, const Right& r) {
--- Uebung3/Expression/Vector.h
+++ Uebung3/Expression/Vector.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <array>
-#include "Expression.h"
 using namespace std;
 
 template < class T, size_t S > 
@@ -13,44 +12,44 @@ public:
 		size_t s = __min(data.size(), S);
 		auto it = data.begin();
 		for (size_t i = 0; i < s; i++) this->at(i) = *it++;
-	};
+	}
+
 	//Standardconstruktor
 	Vector() {}
 
 	//Indexoperator 
 	T operator[](int i) const {
 		return this->at(i);
-	};
+	}
 
 	// Ausgabe-Operator für Output-Streams	
 	friend ostream& operator<<(ostream& os, const Vector& v) {
-		os << "[";
-		for (size_t i = 0; i < S; ++i) { os << ", " << v[i]; }
-		os << "]";
+		os << "v[";
+		for (size_t i = 0; i < S; ++i) { os << "  " << v[i]; }
+		os << "  ]";
 		return os;
-	};
+	}
 	
 	// Zuweisungsoperator
 	template<typename T>
 	Vector& operator = (const T& t) {
-		for (size_t i = 1; i < S; ++i) this->at(i) = t[i];
+		for (size_t i = 0; i < S; ++i) this->at(i) = t[i];
 		return *this;
-	};
+	}
 
 	//Gleichheitsoperator
-	bool operator==(const Vector& v) const {
-		if (S == v.size()) {
-			for (size_t i = 0; i < S; i++)
-			{
-				if (this->at(i) != v[i]) {
+	template<typename T>
+	bool operator==(const T& t) const {
+		if (S == t.size()) {
+			for (size_t i = 0; i < S; i++){
+				if (this->at(i) != t[i]) {
 					return false;
 				}
-
 			}
 		}else {
 			return false;
 		}
 		return true;
-	};
+	}
 
 };
